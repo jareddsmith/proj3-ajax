36d173e24712027db48e5afd93c6d8e191c33058
Jared Smith <Jared Smith>
2016-02-02 16:45:33 -0800

    Testing suite

---
+++ test_app.py
@@ -0,0 +1,112 @@
+def test_app():	
+	min_limits = [15, 15, 15, 11.428, 13.333]
+	max_limits = [34, 32, 30, 28, 26]
+	change_list = [200, 400, 600, 1000, 1300]
+	st_control = [0, 200/34, 825/68, 3835/204, 47245/1428, 828385/18564]
+	end_control = [600/15, 400/11.428, 300/13.333]
+	max_spd = 0
+	min_spd = 0
+	start_time = 0
+	end_time = 0
+	
+	print("Testing is beginning")
+	
+	t_conv = test_conv(124.300808, "mi")
+	
+	print(t_conv)
+		
+	if test_conv(124.300808,"mi") == 200:
+		print("Conversion test passed")
+	else:
+		print("Conversion failed")
+		
+	max_spd,min_spd = test_spdchk(199, change_list, max_limits, min_limits)
+	if max_spd == 34 and min_spd == 15:
+		print("Speed check for 199km passed")
+	else:
+		print("Speed check failed")
+	
+	max_spd,min_spd = test_spdchk(200, change_list, max_limits, min_limits)
+	if max_spd == 32 and min_spd == 15:
+		print("Speed check for 200km passed")
+	else:
+		print("Speed check failed")
+		
+	max_spd,min_spd = test_spdchk(400, change_list, max_limits, min_limits)
+	if max_spd == 30 and min_spd == 15:
+		print("Speed check for 400km passed")
+	else:
+		print("Speed check failed")
+		
+	max_spd,min_spd = test_spdchk(600, change_list, max_limits, min_limits)
+	if max_spd == 28 and min_spd == 11.428:
+		print("Speed check for 600km passed")
+	else:
+		print("Speed check failed")
+	
+	max_spd,min_spd = test_spdchk(1000, change_list, max_limits, min_limits)
+	if max_spd == 26 and min_spd == 13.333:
+		print("Speed check for 1000km passed")
+	else:
+		print("Speed check failed")
+	
+
+    
+def test_conv(dist, dist_unit):
+    #Check if the units selected is miles, if so convert to kilometers.
+	if dist_unit == "mi":
+		dist *= 1.609
+	return dist
+
+    
+def test_spdchk(dist, change_list, max_limits, min_limits):
+    #A for-loop/conditional hybrid to find for the correct speeds to use
+	if dist >= 0:
+		for change in change_list:
+			if dist < change:
+				max_spd = max_limits[change_list.index(change)]
+				min_spd = min_limits[change_list.index(change)]
+				return(max_spd,min_spd)
+	return
+				
+def test_extime(dist, change_list, brev_dist):
+    #Calculates the extra time that is need under certain conditions.
+	extra_time = 0
+	if dist >= 0:
+		for change in change_list:
+			if brev_dist == dist:
+				if dist >= brev_dist:
+					dist = brev_dist
+					if dist == 200 or dist == 1000:
+						extra_time = 10
+					elif dist == 400:
+						extra_time = 20
+	return extra_time
+
+def test_calc(dist,max_spd,min_spd,st_control,end_control):
+    #Calculates the times depending on the distance
+	if dist in range(0,200):
+		start_time = dist/max_spd + st_control[0]
+		end_time = dist/min_spd
+
+	elif dist in range(200,400):
+		start_time = (dist-200)/max_spd + st_control[1]
+		end_time = dist/min_spd
+
+	elif dist in range(400,600):
+		start_time = (dist-400)/max_spd + st_control[2]
+		end_time = dist/min_spd
+
+	elif dist in range(600,1000):
+		start_time = (dist-600)/max_spd + st_control[3]
+		end_time = dist/min_spd + end_control[0]
+
+	elif dist in range(1000,1300):
+		start_time = (dist-1000)/max_spd + st_control[4]
+		end_time = (dist-1000)/min_spd + end_control[0] + end_control[1]
+
+	elif dist > 1300:
+		start_time = (dist-1300)/max_spd + st_control[5]
+		end_time = (dist-600)/min_spd + end_control[0] + end_control[1] + end_control[2]
+		
+	return (start_time,end_time)
