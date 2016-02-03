8470a4e579f53f521c491bc4cb64f54590cd35da
Jared Smith <Jared Smith>
2016-02-02 17:37:51 -0800

    Functional

--- test_app.py
+++ test_app.py
@@ -10,12 +10,10 @@ def test_app():
 	end_time = 0
 	
 	print("Testing is beginning")
+	temp = 200/1.609
+	t_conv = test_conv(temp, "mi")
 	
-	t_conv = test_conv(124.300808, "mi")
-	
-	print(t_conv)
-		
-	if test_conv(124.300808,"mi") == 200:
+	if test_conv(temp,"mi") == 200:
 		print("Conversion test passed")
 	else:
 		print("Conversion failed")
@@ -24,7 +22,7 @@ def test_app():
 	if max_spd == 34 and min_spd == 15:
 		print("Speed check for 199km passed")
 	else:
-		print("Speed check failed")
+		print("Speed check failed for 199km")
 	
 	max_spd,min_spd = test_spdchk(200, change_list, max_limits, min_limits)
 	if max_spd == 32 and min_spd == 15:
@@ -36,21 +34,51 @@ def test_app():
 	if max_spd == 30 and min_spd == 15:
 		print("Speed check for 400km passed")
 	else:
-		print("Speed check failed")
+		print("Speed check failed for 400km")
 		
 	max_spd,min_spd = test_spdchk(600, change_list, max_limits, min_limits)
 	if max_spd == 28 and min_spd == 11.428:
 		print("Speed check for 600km passed")
 	else:
-		print("Speed check failed")
+		print("Speed check failed for 600km")
 	
 	max_spd,min_spd = test_spdchk(1000, change_list, max_limits, min_limits)
 	if max_spd == 26 and min_spd == 13.333:
 		print("Speed check for 1000km passed")
 	else:
-		print("Speed check failed")
+		print("Speed check failed for 1000km")
 	
-
+	extra_time = test_extime(200, change_list, 200)
+	if extra_time == 10:
+		print("Extra time check for -> 200km/1000km passed")
+	else:
+		print("Extra time check for -> 200km/1000km passed")
+		
+	extra_time = test_extime(1000, change_list, 1000)
+	if extra_time == 10:
+		print("Extra time check for 200km/1000km <- passed")
+	else:
+		print("Extra time check for 200km/1000km <- passed")
+		
+	extra_time = test_extime(400, change_list, 400)
+	if extra_time == 20:
+		print("Extra time check for 400km passed")
+	else:
+		print("Extra time check for 400km passed")
+		
+	start_time,end_time = test_calc(170, max_limits[0], min_limits[0], st_control, end_control)
+	if start_time == 5 and end_time == 170/15:
+		print("Time test for 170km passed")
+	else:
+		print("Time test failed: ", test_calc(170, max_limits[0], min_limits[0], st_control, end_control))
+	
+	start_time,end_time = test_calc(200, max_limits[1], min_limits[1], st_control, end_control)
+	if start_time == 200/34 and end_time == 200/15:
+		print("Time test for 200km passed")
+	else:
+		print("Time test failed: ", test_calc(200, max_limits[1], min_limits[1], st_control, end_control))
+	
+	
     
 def test_conv(dist, dist_unit):
     #Check if the units selected is miles, if so convert to kilometers.
@@ -110,3 +138,6 @@ def test_calc(dist,max_spd,min_spd,st_control,end_control):
 		end_time = (dist-600)/min_spd + end_control[0] + end_control[1] + end_control[2]
 		
 	return (start_time,end_time)
+	
+if __name__ == "__main__":
+	test_app()
